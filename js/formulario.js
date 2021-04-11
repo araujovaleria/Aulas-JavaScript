4b76f89d51fc5151e4eb45cd1dd8e75e99346f3b
araujovaleria <araujovaleria.arte@gmail.com>
2021-04-11 10:44:33 -0300

    tela login validacao de campos - estudo

--- js/formulario.js
+++ js/formulario.js
@@ -1,28 +1,15 @@
 
-function validarSenha (){
-    const botao = document.querySelector('#button').value;
-    const senha1 = document.querySelector('#senha1').value;
-    const senha2 = document.querySelector('#senha2').value;
-    if (senha1 == senha2){
-       alert('senha correta')
-    }else{
-        alert('vc errou!')
-    }
-
-}
-
-/*(function () {
-    const botao = document.querySelector('#button').value;
-    const senha1 = document.querySelector('#senha1').value;
-    const senha2 = document.querySelector('#senha2').value;
-    if (senha1 === senha2){
-        alert('senha correta')
-    }else{
-        alert('vc errou!')
-    }
+(function () {
+    const botao = document.querySelector('#button');
 
     botao.addEventListener('click', () =>{
-         
+        const senha1 = document.querySelector('#senha1').value;
+        const senha2 = document.querySelector('#senha2').value;
+        if (senha1 == senha2){
+            alert('senha correta')
+        }else{
+            alert('vc errou!')
+        }
     });
  
-})();*/
+})();
